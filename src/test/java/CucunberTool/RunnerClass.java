0b135c16d06db3acb0070be4943c2340b2fcb0d4
IuliiaI <iuliiaiaremko@gmail.com>
2022-12-08 14:53:53 -0500

    Cucumber as a tool and cucumber Framework Basics

---
+++ src/test/java/CucunberTool/RunnerClass.java
@@ -0,0 +1,19 @@
+package CucunberTool;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "C:\\Users\\iulii\\IdeaProjects\\CucumberBatch14\\src\\test\\java\\CucunberTool\\Login.feature"
+)
+
+
+
+
+
+public class RunnerClass {
+
+}
